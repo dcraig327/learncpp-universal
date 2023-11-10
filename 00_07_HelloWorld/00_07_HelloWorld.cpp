7b9af599390526c263d5a3ac8fe1011c3f402a13
David Craig <37514553+dcraig327@users.noreply.github.com>
2023-11-10 15:42:11 -0800

    Add project files.

---
+++ 00_07_HelloWorld/00_07_HelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿// 00_07_HelloWorld.cpp : Defines the entry point for the application.
+//
+
+#include "00_07_HelloWorld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
